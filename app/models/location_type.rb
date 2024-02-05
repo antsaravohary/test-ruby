daa474df8458e8994086cb5b93bd222880c617c7
antsaravohary <anta.ravohary@gmail.com>
2024-02-05 18:22:42 +0300

    feat: "Add model BaseLocationType"

--- app/models/location_type.rb
+++ app/models/location_type.rb
@@ -3,6 +3,6 @@ class LocationType < ApplicationRecord
 
   belongs_to :company
   has_many :spots, dependent: :destroy
-
+  belongs_to :base_location_type
   enum :nature, { common_areas: 0, housing: 1, other_spots: 2, zone: 2 }
 end
